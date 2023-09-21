7fcfb6cf7f0df5518b55c4819706e109b4daf205
João Vitor Ribeiro <2015ribeirojv@gmail.com>
2023-09-20 21:51:59 -0300

    ProtótipoMenu

---
+++ TCP3_RTS/Assets/Programação/Scripts/MenuPrincipalManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private int Index_Scena;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene(Index_Scena);
+    }
+
+    public void AbrirOpcoes() 
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+
+    public void FecharOpcoes()
+    {
+        painelOpcoes.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("jogo fechou");
+        Application.Quit();
+    }
+
+}
