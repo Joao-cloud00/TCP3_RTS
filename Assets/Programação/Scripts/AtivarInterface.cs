30779236bb1272ca8708a7c2d89a84f706fb3005
João Vitor Ribeiro <2015ribeirojv@gmail.com>
2023-11-01 17:54:40 -0300

    interface Construtor

--- Assets/Programação/Scripts/AtivarInterface.cs
+++ Assets/Programação/Scripts/AtivarInterface.cs
@@ -28,36 +28,36 @@ public class AtivarInterface : MonoBehaviour
                 
                 if (hit.collider.CompareTag("Unit"))
                 {
+                    print("Botao Esquerdo cclicado");
                     prefabInterface.SetActive(true);
                 }
             }
         }
         if(Input.GetMouseButtonDown(1))
         {
-            // Lança um raio a partir da posição do cursor do mouse
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            //// Lança um raio a partir da posição do cursor do mouse
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hit;
 
-            // Verifica se o raio atingiu algo
-            if (Physics.Raycast(ray, out hit))
-            {
-                // Verifique se o objeto atingido esta com a tag Grid
-                if (hit.collider.CompareTag("Grid"))
-                {
-                    // Obtém a posição do clique em coordenadas de mundo
-                    Vector3 clickPosition = hit.point;
+            //// Verifica se o raio atingiu algo
+            //if (Physics.Raycast(ray, out hit))
+            //{
+            //    // Verifique se o objeto atingido esta com a tag Grid
+            //    if (hit.collider.CompareTag("Placement"))
+            //    {
+            //        // Obtém a posição do clique em coordenadas de mundo
+            //        Vector3 clickPosition = hit.point;
 
-                    // Calcula a coordenada (x, y) com base na posição do clique
-                    int a = Mathf.FloorToInt(clickPosition.x);
-                    int b = Mathf.FloorToInt(clickPosition.z);
+            //        // Calcula a coordenada (x, y) com base na posição do clique
+            //        int a = Mathf.FloorToInt(clickPosition.x);
+            //        int b = Mathf.FloorToInt(clickPosition.z);
 
-                    //destino = new Vector3(a+1.5f, 0.5f, b);
-                    //transform.position = destino;
-                }
-            }
+            //        //destino = new Vector3(a+1.5f, 0.5f, b);
+            //        //transform.position = destino;
+            //    }
+            //}
+            print("Botão direito clicado");
             prefabInterface.SetActive(false);
-            //Vector3 posicaoNova = (destino - posInit).normalized;
-            //transform.Translate(posicaoNova * velocidade * Time.deltaTime);
         }
 
     }
