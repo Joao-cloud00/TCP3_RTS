9298f7d0040b601e863b41cae22503a3f3996c97
João Vitor Ribeiro <2015ribeirojv@gmail.com>
2023-11-15 20:59:29 -0300

    v.0.2
    Videos 1 e 2 com problema, não esta sselecionando as unidades

---
+++ Assets/NOVOppc/Scripts/BaseUnit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseUnit : MonoBehaviour
+{
+    public Projector projector;
+    private bool _isSelected;
+
+    public bool IsSelected
+    {
+        get { return _isSelected; }
+        set 
+        {
+            if (value)
+            {
+                projector.enabled = true;
+            }
+            else
+            {
+                projector.enabled = false;
+            }
+
+            _isSelected = value;
+        }
+    }
+
+    public void Start()
+    {
+        UnitController.AddBaseUnitToList(this);
+    }
+}
